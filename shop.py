96cc6f54c97163bd9aba5a3b7aaad91eed222d5c
demid <demid090605@outlook.com>
2025-03-06 18:25:51 +0700

    Fix: - Add new function that simplify text animating and code reading 	изменено:      shop.py

--- shop.py
+++ shop.py
@@ -5,26 +5,42 @@ color = ( '#710008', '#FF5964')
 #color = ('#241024', '#6D326D')
 #color = ('#077C63', "#95F9E3")
 #color = ('#685307', '#F4D35E')
+
+shop_text_dict = ("Магазин", "Заказы", "Заказы групп", "Обучение")
+
 class ShopAnimation(Scene):
     def construct(self):
         self.camera.background_color = convert_color(color[0])
-        text = Text("Магазин", color=convert_color(color[1]), font_size=30, font="Russo One")
-        text.shift(LEFT * -6)
-        text.shift(DOWN * 3)
 
         rect = Rectangle(width=4, height=9, fill_opacity=1, color=convert_color(color[1]))
         rect.shift(LEFT * 6)
-        # the beginning of animation
-        self.play(Write(text), Create(rect))
+        self.play(Create(rect))
         self.play(Wait(5))
 
-        # idle animation
+        for i in range(len(shop_text_dict)):
+            self.add_text_and_move(shop_text_dict[i])
+
+    def add_text_and_move(self, animated_text : str):
+
+        text = Text(animated_text, color=convert_color(color[1]), font_size=30, font="Russo One")
+
+        text.shift(LEFT * -6)
+        text.shift(DOWN * 3)
+
         self.play(text.animate.shift(LEFT * 20), run_time=10)
 
         text.shift(RIGHT * 25)
-        self.play(text.animate.shift(LEFT * 5), run_time=10)
+
+        if (len(animated_text) >= 10):
+            self.play(text.animate.shift(LEFT * 7), run_time=10)
+        else:
+            self.play(text.animate.shift(LEFT * 5), run_time=10)
+
+        self.play(Unwrite(text))
+        self.play(Wait(5))
 
 
 def convert_color(color : str):
     return rgb_to_color(hex_to_rgb(color))
+
 
